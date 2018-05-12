c2688845d36f27b1f9e95d8cda9026a7c97090e3
lliu <lliu@cmcrc.com>
2018-05-12 16:07:24 +1000

    update

--- coname.py
+++ coname.py
@@ -7,11 +7,10 @@ import sys
 from collections import Counter, defaultdict
 from datetime import datetime as dt
 from unicodedata import normalize
-from zipfile import ZipFile
-
-from bs4 import BeautifulSoup as bs
+import pandas as pd
 from fuzzywuzzy import fuzz
 from nltk.tokenize import sent_tokenize
+from concurrent.futures import ProcessPoolExecutor
 
 abbr = [('Inc','Incorporated'),('Incorp','Incorporated'), ('Assn','Association'),
         ('CORP', 'Corporation'), ('CO', 'Company'), ('LTD', 'Limited'), ('BANCORP', 'Banking Corporation'),
@@ -62,5 +61,77 @@ def first_letters(name): # get the first letter of firm names in order to match 
             return
         return ''.join(ls) + found_suffix[0]
 
-def set_remove_punc(name):
-    return set(re.split('\s+',re.sub(r'[^\w\s]','',name)))
+def remove_punc(name):
+    return re.sub(r'[^\w\s]','',name)
+def not_has_adpos(word):
+    if (' a ' in word) or ('In ' in word) or (' in ' in word) or ( ' against ' in word):
+        return False
+    else:
+        return True
+
+def match(x, y):
+    ''' matching between two names and write to file...
+    '''
+    x, y = abbr_adj(x), abbr_adj(y)
+    x_words, y_words = re.split('\s+',remove_punc(x)), re.split('\s+',remove_punc(y))
+    ''' Scenario 1 : the neighbour of matched words must be matched, unless there is no neighbours:
+    '''
+    matching_pos_x,matching_pos_y = None, None
+    threshold = 94
+    identified = False
+    # create dict to index sequences
+    x_seq = {n:x for n,x in enumerate(x_words)} 
+    y_seq = {n:y for n,y in enumerate(y_words)}
+    for id_x, x_word in x_seq.items():
+        for id_y, y_word in y_seq.items():
+            if fuzz.token_set_ratio(x_word, y_word) > threshold and len(x_word)>1: # allow one mistake per five letters
+                identified = True
+                if x_word == x_words[0] and y_word == y_words[-1] and len(x_words)>1 and not_has_adpos(y_word):
+                    return
+                if y_word == y_words[0] and x_word == x_words[-1] and len(y_words)>1 and not_has_adpos(x_word):
+                    return
+                if (id_x-1 in x_seq) and (id_y-1 in y_seq): 
+                    if fuzz.token_set_ratio(x_seq[id_x-1], y_seq[id_y-1]) < threshold: # The previous words must match
+                        if not (matching_pos_x and matching_pos_y): # if no previous matched words, exclude...
+                            return
+                        if matching_pos_x - id_x != 1 and matching_pos_y - id_y != 1:
+                            return # if previous word does not match, and last match is not close, exclude it ...
+                if (id_x+1 in x_seq) and (id_y+1 in y_seq):
+                    if fuzz.token_set_ratio(x_seq[id_x+1], y_seq[id_y+1]) < threshold:
+                        if (id_x+2 in x_seq) and (id_y+2 in y_seq):
+                            if fuzz.token_set_ratio(x_seq[id_x+2], y_seq[id_y+2]) > threshold:
+                                continue # if next word does not match, only the next next word match the test can continue ...
+                            else:
+                                return
+                        else:
+                            return                    
+                matching_pos_x,matching_pos_y = id_x, id_y
+    if identified:
+        return True
+def unpacking(mp_file):
+    lst = []
+    with open(f'.\\match_pool\\{mp_file}') as f:
+        rd = csv.reader(f)
+        for m_index, m_name, s_index, s_name, m_adj, s_adj, type_ in rd:
+            if type_ == 'full':
+                if match(m_name, s_name):
+                    lst.append([m_index, m_name, s_index, s_name, type_])
+    return lst
+
+
+mp = os.listdir('.\\match_pool\\')
+wastime = dt.now()
+print(wastime)
+def main():
+    with ProcessPoolExecutor() as e:
+        with open('__coname__.csv','w',newline='') as w:
+            wr = csv.writer(w)
+            for mp_file, result in zip(mp, e.map(unpacking, mp,chunksize=1000)):
+                print(mp_file)
+                if result is not None:
+                    for matched in result:
+                        wr.writerow(matched)
+if __name__ == '__main__':
+    main()
+
+print(dt.now(), (dt.now() - wastime).total_seconds()/60)
