3d0ab0c9fda98e18ffdd3f012a49c7dace647537
Leo Liu <auleo.liu@gmail.com>
2021-03-31 15:31:07 +1100

    add b/w list

--- matcher.py
+++ matcher.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import argparse,csv,math,os,re,string,sys,json,pkg_resources,pathlib
+import argparse,csv,math,os,re,string,sys,json,pkg_resources,pathlib,functools
 from collections import Counter, defaultdict
 from multiprocessing import Pool
 from datetime import datetime as dt
@@ -14,20 +14,41 @@ from nltk.tokenize import sent_tokenize
 
 cutoff=80
 
-abbr = [('the',''),('and',''),('of',''),('for',''),('llc','llc'),
-        ('Inc', 'incorp'), ('Incorporated','incorp'),
+abbr1 = [
+        # corporation related words and some uninformative words
+        ('the',''),('and',''),('of',''),('for',''),
+        ('llc','llc'), ('Inc', 'incorp'), ('Incorporated','incorp'),
         ('CO', 'company'), ('COS', 'Company'),('companies', 'Company'),
+        ('comapany', 'company'),
         ('cor', 'incorp'),('CORP', 'incorp'),('corporation', 'incorp'),
-        ('coporation', 'incorp'),
-        ('corpor', 'incorp'),('corporat', 'incorp'),('corporat', 'incorp'),
+        ('coporation', 'incorp'), ('corpor', 'incorp'),
+        ('corporat', 'incorp'),('corporat', 'incorp'),
         ('corporate', 'incorp'),('corporatin', 'incorp'),
         ('LTD', 'limited'),('limit', 'limited'),('limite', 'limited'),
         ('company incorp', 'incorp'),('incorp incorp', 'incorp'),
         ('company limited', 'limited'),('incorp limited', 'limited'),
         ('Assn', 'Association'),('Assoc', 'Association'),
-        ('intl', 'international'), ('gbl','global'),('natl','national'),
-        ('nat','national'),
-        ('int', 'international'),('univ','university'),
+        ('intl', 'international'),
+        ('gbl','international'), ('global','international'),
+        ('natl','national'), ('nat','national'),
+        ('int', 'international'),
+        ('&', 'and'), ('L\.P','LP'),('L\.L\.P','LLP'),('S\.A','SA'),('S\.p\.A','SPA'),
+        ('u s a','usa'), ('usa','usa'), ('u s','usa'), ('us','usa'),
+        # Japanese suffix
+        ('(?<!^)kk',''),('(?<!^)gk',''),('(?<!^)yk',''),('(?<!^)gmk',''),
+        ('(?<!^)gsk',''),('(?<!^)nk',''),('(?<!^)tk',''),
+        # Germany suffix
+        ('(?<!^)ev',''),('(?<!^)rv',''),('(?<!^)kgaa',''),('gmbh co',''),('(?<!^)ag co',''),('(?<!^)se co',''),
+        ('gmbh',''),('(?<!^)ag',''),('(?<!^)se',''),('(?<!^)ug',''),
+        # French suffix
+        ('(?<!^)sep',''),('(?<!^)snc',''),('(?<!^)scs',''),('(?<!^)sca',''),
+        ('(?<!^)sci',''),('(?<!^)sarl',''), ('(?<!^)eurl',''),('(?<!^)sa',''),
+        ('(?<!^)scop',''),(r'sas$',''),(r'sasu$',''),
+        # Swedish suffix
+        (r'ab$',''),(r'lm$','')]
+
+abbr2 = [ # informative words
+        ('univ','university'),
         ('MOR', 'Mortgage'), ('Banc', 'BankCorp'),('bk', 'BankCorp'),
         ('bancshares ', 'bankcorp'),('bankshares ', 'bankcorp'),
         ('stores','store'),('brand','brands'),('gen','general'),
@@ -44,7 +65,8 @@ abbr = [('the',''),('and',''),('of',''),('for',''),('llc','llc'),
         ('INDS', 'industries'), ('industry', 'industries'), ('indl', 'industries'),
         ('IND', 'industries'),('res','research'),('dev','development'),
         ('IP', ''), ('intellectual property', ''),('intellectual properties', ''),
-        ('intellectual', ''),('patents',''),('patent',''),('trademark',''),('trademarks',''),
+        ('intellectual', ''),(r'(?!^)patents',''),(r'(?!^)patent',''),
+        (r'(?!^)trademark',''),(r'(?!^)trademarks',''),
         ('marketing',''),
         ('property', 'properties'), ('Mort', 'Mortgage'), ('Thr', 'Through'),
         ('Sec', 'Securities'), ('BANCORPORATION', 'BankCorp'),
@@ -57,30 +79,17 @@ abbr = [('the',''),('and',''),('of',''),('for',''),('llc','llc'),
         ('Product', 'products'), ('svcs','services'),('service','services'),
         ('production','productions'),('saving','savings'),('svgs','savings'),
         ('ln','loan'),('electronic','electronics'),('elect','electronics'),
-        ('electrs','electronics'),
-        ('inst','institution'),
+        ('electrs','electronics'), ('inst','institution'),
         ('motors','motor'),
         ('machine','machines'),('machs','machines'),('teleg','telegraph'),
         ('tel','telephone'),('tel','telephone'),('ry','railway'),
         ('american','america'),('AMER','america'),('AMERN','america'),
-        ('&', 'and'), ('L\.P','LP'),('L\.L\.P','LLP'),('S\.A','SA'),('S\.p\.A','SPA'),
-        ('u s a','usa'), ('usa','usa'), ('u s','usa'), ('us','usa'),
-        # Japanese suffix
-        ('(?<!^)kk',''),('(?<!^)gk',''),('(?<!^)yk',''),('(?<!^)gmk',''),
-        ('(?<!^)gsk',''),('(?<!^)nk',''),('(?<!^)tk',''),
-        # Germany suffix
-        ('(?<!^)ev',''),('(?<!^)rv',''),('(?<!^)kgaa',''),('gmbh co',''),('(?<!^)ag co',''),('(?<!^)se co',''),
-        ('gmbh',''),('(?<!^)ag',''),('(?<!^)se',''),('(?<!^)ug',''),
-        # French suffix
-        ('(?<!^)sep',''),('(?<!^)snc',''),('(?<!^)scs',''),('(?<!^)sca',''),
-        ('(?<!^)sci',''),('(?<!^)sarl',''), ('(?<!^)eurl',''),('(?<!^)sa',''),
-        ('(?<!^)scop',''),(r'sas$',''),(r'sasu$',''),
-        # Swedish suffix
-        (r'ab$',''),(r'lm$','')
+        ('phillip','philip')
         ]
 
+abbr = abbr1 + abbr2
 
-common_abbr = set([x for _,x in abbr if x !=''])
+common_abbr = set([x for _,x in abbr1 if x !=''])
 # for some abbreviations, we have to hard code it.
 hardcode = [('HP', 'HEWLETT PACKARD'),('IBM','international business machines'),
             (r'DU PONT$','DU PONT EI DE NEMOURS'),
@@ -98,10 +107,6 @@ suffix = set(['incorp', 'llc', 'company', 'limited', 'trust','lp','llp','sa','sp
              )
 suffix_regex = '|'.join(suffix)
 
-removal_regex = re.compile('|'.join(
-    [r'\band\b',r'\bof\b',r'\bfor\b',r'\bholdings\b', r'\bholding\b', r'\bgroup\b',
-     r'\benterprises\b', r'\binternational\b',r'\bglobal\b']))
-
 def loc(f):
     return os.path.join(pathlib.Path(__file__).parent.absolute(),f)
 
@@ -109,30 +114,39 @@ eng = set(json.load(open('words_dictionary.json')).keys())
 eng = eng | set([x.lower().strip() for x in (open(loc('surname.txt')).readlines())])
 eng = eng | set([x.lower().strip() for x in (open(loc('firstname.txt')).readlines())])
 
-
-def abbr_adj(name):  # replace abbr to full
-    for string, adj_string in abbr+hardcode:
+def _abbr_adj(name,l):  # replace abbr to full
+    for string, adj_string in l:
         name = re.sub('(?<!\w)' + string + '(?!\w)',
                       ' ' + adj_string,
                       name,
                       flags=re.IGNORECASE)
     return name.strip().lower()
 
+abbr_adj = functools.partial(_abbr_adj,l=abbr+hardcode)
+abbr_suffix_adj = functools.partial(_abbr_adj,l=abbr1+hardcode)
+abbr_extra_adj = functools.partial(_abbr_adj,l=abbr2+hardcode)
 
-def suffix_adj(name):  # Remove suffix
+def disamb(name):  # Remove suffix
+    name = abbr_adj(name)
     for string in suffix:
-        name = re.sub(
-            '(?<!\w)' + string +
-            '(?!\w)',  # The string has to be after some punctuations or space.
+        if not name.startswith(string):
+            name = re.sub(
+            r'\b' + string +
+            r'\b',  # The string has to be after some punctuations or space.
             '', name, flags=re.IGNORECASE)
     return name.strip()
 
 
 def name_preprocessing(z):
-    z = z.replace('-REDH', '').replace('-OLD', '').replace('-NEW', '')
-    z = ' '.join(re.findall(r'[\w\d]+',z))
-    z = re.sub('The ', '', z, flags=re.I)
     z = z.lower()
+    z = z.replace('-redh', '').replace('-old', '').replace('-new', '')
+    z = re.sub(r'(?=\w+)our\b',r'or',z)
+    z = re.sub(r'(?=\w+)tt\b',r't',z)
+    #  z = re.sub(r'(?=(\w+))([a-zA-Z])\2?',r'\2',z)
+    z = re.sub(r'(?=\w+)er\b',r'ers',z) # to not match e.g. glove vs glover
+    z = z.replace('`','').replace('& company','').replace('& companies','')
+    z = re.sub('\bthe\b', '', z)
+    z = ' '.join(re.findall(r'[\w\d]+',z))
     # combining single words...
     a = ''.join(re.findall(r'\b\w\s\b',z))
     if a:
@@ -144,9 +158,8 @@ def name_preprocessing(z):
                          ('vi',''),('vii',''),('viii',''),('ix',''),('x','')]:
         z = re.sub('(?<!\w)' + string + '(?!\w)',
                       ' ' + adj_string, z, flags=re.IGNORECASE)
-    z = abbr_adj(z)
+    z = abbr_suffix_adj(z)
     return z.strip().lower()
-
 
 def check_double(a,b):
     ''' account for double ('BALL & BALL CARBURETOR COMPANY','BALL CORP')'''
@@ -160,16 +173,35 @@ def check_double(a,b):
                         break
             else:
                 return False
-def remove_meaningless(s):
-    return removal_regex.sub('',s).strip()
+def remove_meaningless(name):
+    for x in ['and', 'of','for','holdings','holding', 'group',
+              'enterprises', 'international','global']:
+        if not name.startswith(x):
+            name = re.sub(r'\b'+x+r'\b','',name).strip()
+    return name.strip()
 
 ban_list = ('organization','organization','academy','university','commission')
 
-def match(a,b):
+def match(a,b,c,d):
+    # part 1: high similarity scores treatment
+    if len(c.split()) > 2 or len(c.split()) > 2: # long names more forgiving
+        if fuzz.token_sort_ratio(c,d)>91:
+            return True
+    else:
+        if fuzz.token_sort_ratio(c,d)>96:
+            return True
+    if not (set(a.split()) - suffix): # if a only has suffix left, bad ...
+        return False
+    if not (set(b.split()) - suffix): # if b only has suffix left, bad ...
+        return False
+    if (fuzz.token_sort_ratio(a,b)>96) or (fuzz.ratio(sorted(a),sorted(b))==100):
+        return True
     for w in ban_list:
         if w in a:
             return False
+    # part 2: low simiarity, try more cleaning ...
     good_y = set()
+    has_bad_x = False
     pos_y = dict()
     # notice that x is CRSP firms (which is more standard) and y is target names
     x,y = remove_meaningless(b).split(),remove_meaningless(a).split()
@@ -178,57 +210,77 @@ def match(a,b):
     if len(x)==1:
         if (x[0] in eng) or (len(x[0])<5):
             return False
-    for wx in x:
+
+    for m,wx in enumerate(x,start=1):
         match_wx = False
         for n,wy in enumerate(y,start=1):
             if wy not in pos_y:
                 pos_y[wy] = n
-            if (len(x) == len(y)) and (len(x)>4):
-                threshold = 75
+            if n==1:
+                threshold = 92 # more strict if first word (letter 5 vs 6)
             else:
                 threshold = 89
-            if fuzz.ratio(wx,wy)>threshold and wx[0]==wy[0]: # first letter must match
+            if (len(x) == len(y)) and (len(x)>4):
+                threshold = 75
+            if ((fuzz.ratio(wx,wy)>threshold) and (wx[0]==wy[0]) # first letter must match
+                    and (wy[-1] not in '1234567890')): # last char is not a number
                 match_wx = True
                 good_y.add(wy)
         if not match_wx and (wx not in suffix): # every word in X must have a match in Y
-            return False
+            has_bad_x = True
+        if not match_wx and m==1: # First X word much match no matter what
+            has_bad_x = True
 
     if check_double(x,y) is False:
         return False
     if check_double(y,x) is False:
         return False
 
-    __good_y= set(good_y) - common_abbr - suffix
-
-    if len(__good_y - eng)>0:
-        if len(__good_y)*len([w for q in __good_y for w in q if w in string.ascii_letters])>20:
-            return True
-    bad_y = set(y) - set(good_y) - suffix
-    if len(bad_y)==0: # no additional words except for suffix in Y means good match
+    __good_y= good_y - common_abbr - suffix
+    #  if len(__good_y - eng)>0:
+    if len(__good_y)*len([w for q in __good_y for w in q if w in string.ascii_letters])>20:
         return True
+    # match fail if has bad X and did not pass the multi-phrase test above ...
+    if has_bad_x is True:
+        return False
+
+    # check unique words in bad_y after removing suffix
+    # (always keep first word as it is informative such as 'international' ...)
+    bad_y = ((set(y) - suffix) | set([y[0]])) - good_y
+    if len(bad_y)==0: # no additional words except for suffix in Y means good match
+        return 1
     for bad_wy in bad_y:
         if pos_y[bad_wy]<=len(x): # all additional words in Y must appear after X
             return False
+
+    remaining_x = set(x) - common_abbr - suffix
+    if not remaining_x: # if nothing left in x
+        return False
+    if len(remaining_x)==1: # if after remove things, the x is a letter, bad match
+        remaining_wx = next(iter(remaining_x))
+        if len(remaining_wx)==1: # or remaining_wx in eng:
+            return False
     return True
+
+def match_test(a,b):
+    c,d = name_preprocessing(a),name_preprocessing(b)
+    a,b = disamb(c),disamb(d)
+    score = fuzz.token_set_ratio(a,b)
+    print(a,'  |||||  ',b)
+    if score>cutoff:
+        return match(a,b,c,d)
+    else:
+        print('failed at cutoff',cutoff,' is',score)
 
 def unpacking(main_row):
     lst = []
-    main_index, main_name, main_disamb, main_wosuf = main_row
-    for base_index, base_name, base_disamb, base_wosuf in base_.values:
-        if fuzz.token_set_ratio(main_wosuf,base_wosuf)>cutoff:
-            if match(main_disamb, base_disamb):
+    main_index, main_name, main_pre, main_disamb= main_row
+    for base_index, base_name, base_pre, base_disamb in base_.values:
+        if fuzz.token_set_ratio(main_disamb,base_disamb)>cutoff:
+            if match(main_disamb, base_disamb, main_pre, base_pre):
                 lst.append([main_index, main_name, base_index, base_name,
                             fuzz.token_set_ratio(main_disamb, base_disamb)])
     return lst
-
-def match_test(a,b):
-    a,b = name_preprocessing(a),name_preprocessing(b)
-    score = fuzz.token_set_ratio(suffix_adj(a),suffix_adj(b))
-    print(suffix_adj(a),suffix_adj(b))
-    if score>cutoff:
-        return match(a,b)
-    else:
-        print('failed at cutoff',cutoff,' is',score)
 
 def main():
     with Pool() as p:
@@ -255,10 +307,10 @@ if __name__ == '__main__':
     base_ = pd.read_csv(basefile).dropna()
     main_ = pd.read_csv(filename).dropna()
     # disambiguation
-    base_['disamb'] = base_[base_.columns[1]].map(name_preprocessing)
-    main_['disamb'] = main_[main_.columns[1]].map(name_preprocessing)
-    base_['wo_suffix'] = base_['disamb'].map(suffix_adj)
-    main_['wo_suffix'] = main_['disamb'].map(suffix_adj)
+    base_['pre_proc'] = base_[base_.columns[1]].map(name_preprocessing)
+    main_['pre_proc'] = main_[main_.columns[1]].map(name_preprocessing)
+    base_['disamb'] = base_['pre_proc'].map(disamb)
+    main_['disamb'] = main_['pre_proc'].map(disamb)
 
     wastime = dt.now()
     print(wastime,'start now ...')
