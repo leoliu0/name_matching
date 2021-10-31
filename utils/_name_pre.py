c09148ef27af1575191972dca2ac1ab078f28c8d
Leo Liu <auleo.liu@gmail.com>
2021-10-31 15:26:20 +1100

    preprocess

--- utils/_name_pre.py
+++ utils/_name_pre.py
@@ -60,6 +60,6 @@ def name_preprocessing(z):
                 counter += 1
 
     if len(set([b for a, b in abbr if b != '']) & set(ws)) == 0:
-        if counter >= 2:
+        if counter >= 2 and counter/len(set(ws)-set(['matchit']))>=0.6:
             return
     return z.strip().lower()
